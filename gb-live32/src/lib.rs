9ee5773c61cf5cef15bf5853417d8e99c0462c1f
Joonas Javanainen <joonas.javanainen@gmail.com>
2018-06-16 23:23:05 +0300

    CLI program

---
+++ gb-live32/src/lib.rs
@@ -0,0 +1,202 @@
+extern crate bufstream;
+extern crate cobs;
+#[macro_use]
+extern crate failure;
+extern crate rand;
+extern crate serialport;
+
+use bufstream::BufStream;
+use rand::{FromEntropy, RngCore};
+use rand::rngs::SmallRng;
+use serialport::SerialPort;
+use std::io::{self, BufRead, Read, Write};
+use std::time::Duration;
+
+#[derive(Fail, Debug)]
+pub enum Gbl32Error {
+  #[fail(display = "Handshake failed")]
+  Handshake,
+  #[fail(display = "COBS decode error")]
+  Decode,
+  #[fail(display = "Protocol error: {}", _0)]
+  Protocol(String),
+  #[fail(display = "Serial error: {}", _0)]
+  Serial(#[cause] serialport::Error),
+  #[fail(display = "IO error: {}", _0)]
+  Io(#[cause] io::Error),
+}
+
+pub struct Gbl32 {
+  port: BufStream<Box<SerialPort>>,
+  read_buffer: Vec<u8>,
+  write_buffer: Box<[u8]>,
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub struct Status {
+  pub unlocked: bool,
+  pub passthrough: bool,
+  pub reset: bool,
+}
+
+impl Gbl32 {
+  pub fn from_port(mut port: Box<SerialPort>) -> Result<Gbl32, Gbl32Error> {
+    port
+      .set_timeout(Duration::from_millis(200))
+      .map_err(Gbl32Error::Serial)?;
+    let mut gbl32 = Gbl32 {
+      port: BufStream::new(port),
+      read_buffer: Vec::new(),
+      write_buffer: vec![0; 1024].into_boxed_slice(),
+    };
+    let mut rng = SmallRng::from_entropy();
+    let mut handshake = vec![0; 8];
+    let mut errors = 0;
+    loop {
+      rng.fill_bytes(&mut handshake);
+      match gbl32.ping(&handshake) {
+        Ok(success) => {
+          if success {
+            break;
+          }
+          errors += 1;
+        }
+        Err(Gbl32Error::Decode) | Err(Gbl32Error::Protocol(_)) => errors += 1,
+        Err(Gbl32Error::Io(ref e)) if e.kind() == io::ErrorKind::TimedOut => errors += 1,
+        Err(e) => return Err(e),
+      }
+      if errors > 10 {
+        return Err(Gbl32Error::Handshake);
+      }
+    }
+    Ok(gbl32)
+  }
+  fn request_response(
+    &mut self,
+    cmd: u8,
+    msg: &[u8],
+    expected_len: usize,
+  ) -> Result<&[u8], Gbl32Error> {
+    let mut payload = Vec::from(msg);
+    payload.push(cmd);
+
+    let encoded_len = cobs::encode(&payload, &mut self.write_buffer);
+    self.write_buffer[encoded_len] = 0x00;
+    self
+      .port
+      .write_all(&self.write_buffer[0..(encoded_len + 1)])
+      .map_err(Gbl32Error::Io)?;
+    self.port.flush().map_err(Gbl32Error::Io)?;
+
+    self.read_buffer.clear();
+    self
+      .port
+      .read_until(0x00, &mut self.read_buffer)
+      .map_err(Gbl32Error::Io)?;
+    self.read_buffer.pop();
+
+    let decoded_len = cobs::decode_in_place(&mut self.read_buffer).map_err(|_| Gbl32Error::Decode)?;
+    self.read_buffer.truncate(decoded_len);
+
+    let response_cmd = self.read_buffer.pop();
+    let response_result = self.read_buffer.pop();
+
+    match response_cmd {
+      Some(response_cmd) if cmd == response_cmd => (),
+      Some(response_cmd) => {
+        return Err(Gbl32Error::Protocol(format!(
+          "Command byte mismatch {} vs {}",
+          response_cmd, cmd
+        )))
+      }
+      None => return Err(Gbl32Error::Protocol("Missing command byte".to_string())),
+    }
+    match response_result {
+      Some(0xFF) => {
+        let len = self.read_buffer.len();
+        if len == expected_len {
+          Ok(&self.read_buffer)
+        } else {
+          Err(Gbl32Error::Protocol(format!(
+            "Expected {} bytes in response, got {}",
+            expected_len, len
+          )))
+        }
+      }
+      Some(0xFE) => Err(Gbl32Error::Protocol(
+        String::from_utf8_lossy(&self.read_buffer).to_string(),
+      )),
+      Some(result) => Err(Gbl32Error::Protocol(format!("Result {:02x}", result))),
+      None => Err(Gbl32Error::Protocol("Missing result byte".to_string())),
+    }
+  }
+  pub fn ping(&mut self, data: &[u8]) -> Result<bool, Gbl32Error> {
+    if data.len() != 8 {
+      return Err(Gbl32Error::Protocol(format!(
+        "Expected 8 bytes for ping, got {}",
+        data.len()
+      )));
+    }
+    let response = self.request_response(0x01, data, 8)?;
+    Ok(response == data)
+  }
+  pub fn get_version(&mut self) -> Result<(u8, u8), Gbl32Error> {
+    let data = self.request_response(0x02, &[], 2)?;
+    Ok((data[0], data[1]))
+  }
+  pub fn get_status(&mut self) -> Result<Status, Gbl32Error> {
+    let data = self.request_response(0x03, &[], 3)?;
+    Ok(Status {
+      unlocked: data[0] != 0x00,
+      passthrough: data[1] != 0x00,
+      reset: data[2] != 0x00,
+    })
+  }
+  pub fn set_unlocked(&mut self, value: bool) -> Result<(), Gbl32Error> {
+    self.request_response(0x04, &[value as u8], 0)?;
+    Ok(())
+  }
+  pub fn set_passthrough(&mut self, value: bool) -> Result<(), Gbl32Error> {
+    self.request_response(0x05, &[value as u8], 0)?;
+    Ok(())
+  }
+  pub fn set_reset(&mut self, value: bool) -> Result<(), Gbl32Error> {
+    self.request_response(0x06, &[value as u8], 0)?;
+    Ok(())
+  }
+  pub fn read_block(&mut self, addr_h: u8) -> Result<&[u8], Gbl32Error> {
+    let data = self.request_response(0x07, &[addr_h], 256)?;
+    Ok(data)
+  }
+  pub fn write_block(&mut self, addr_h: u8, data: &[u8]) -> Result<(), Gbl32Error> {
+    if data.len() != 256 {
+      return Err(Gbl32Error::Protocol(format!(
+        "Expected 256 bytes for writing, got {}",
+        data.len()
+      )));
+    }
+    let mut payload = Vec::with_capacity(257);
+    payload.push(addr_h);
+    payload.extend(data);
+    self.request_response(0x08, &payload, 0)?;
+    Ok(())
+  }
+  pub fn write_all(&mut self, data: &[u8]) -> Result<(), Gbl32Error> {
+    if data.len() != 0x8000 {
+      return Err(Gbl32Error::Protocol(format!(
+        "Expected 32768 bytes for writing, got {}",
+        data.len()
+      )));
+    }
+    self.request_response(0x09, &[], 0)?;
+    self.port.write_all(data).map_err(Gbl32Error::Io)?;
+    self.port.flush().map_err(Gbl32Error::Io)?;
+    Ok(())
+  }
+  pub fn read_all(&mut self) -> Result<Vec<u8>, Gbl32Error> {
+    self.request_response(0x0a, &[], 0)?;
+    let mut buf = vec![0; 0x8000];
+    self.port.read_exact(&mut buf).map_err(Gbl32Error::Io)?;
+    Ok(buf)
+  }
+}
---
+++ gb-live32/src/main.rs
@@ -0,0 +1,229 @@
+#[macro_use]
+extern crate clap;
+#[macro_use]
+extern crate failure;
+extern crate gb_live32;
+extern crate itertools;
+#[macro_use]
+extern crate log;
+extern crate rand;
+extern crate serialport;
+extern crate simplelog;
+
+use clap::{Arg, ArgMatches};
+use failure::Error;
+use gb_live32::Gbl32;
+use rand::{FromEntropy, RngCore};
+use rand::rngs::SmallRng;
+use serialport::SerialPortType;
+use simplelog::{LevelFilter, TermLogger};
+use std::ffi::{OsStr, OsString};
+use std::fs::File;
+use std::io::{self, Read, Write};
+use std::process;
+use std::thread;
+
+fn scan_ports() -> Result<Vec<OsString>, Error> {
+  let ports = serialport::available_ports()?
+    .into_iter()
+    .filter_map(|port| {
+      if let SerialPortType::UsbPort(info) = port.port_type {
+        let name = port.port_name;
+        let manufacturer = info.manufacturer.as_ref().map_or("", String::as_ref);
+        let product = info.product.as_ref().map_or("", String::as_ref);
+        let serial_number = info.serial_number.as_ref().map_or("", String::as_ref);
+        if info.vid == 0x16c0 && info.pid == 0x05e1 && manufacturer == "gekkio.fi"
+          && product == "GB-LIVE32"
+        {
+          info!("Detected device: {} ({:04x}:{:04x}, manufacturer=\"{}\", product=\"{}\", serial=\"{}\")",
+            name, info.vid, info.pid, manufacturer, product, serial_number);
+          Some(name.into())
+        } else {
+          info!("Skipping device: {} ({:04x}:{:04x}, manufacturer=\"{}\", product=\"{}\", serial=\"{}\")",
+            name, info.vid, info.pid, manufacturer, product, serial_number);
+          None
+        }
+      } else {
+        None
+      }
+    })
+    .collect::<Vec<_>>();
+  Ok(ports)
+}
+
+#[derive(Clone, Debug)]
+enum Operation {
+  Upload(Vec<u8>),
+  Status,
+}
+
+fn worker(port: &OsString, operation: Operation) -> Result<(), Error> {
+  let name = port.to_string_lossy();
+  let port = serialport::open(&port)?;
+  info!("{}: Connecting...", name);
+  let mut gbl32 = Gbl32::from_port(port)?;
+
+  let version = gbl32.get_version()?;
+  if version != (2, 0) {
+    bail!("{}: Unsupported version v{}.{}", name, version.0, version.1);
+  }
+  info!("{}: Connected (v{}.{})", name, version.0, version.1);
+  unlock_if_necessary(&name, &mut gbl32)?;
+
+  match operation {
+    Operation::Upload(data) => {
+      assert!(data.len() == 32768);
+      gbl32.set_reset(true)?;
+      gbl32.set_passthrough(false)?;
+
+      gbl32.write_all(&data)?;
+
+      gbl32.set_passthrough(true)?;
+      gbl32.set_reset(false)?;
+      info!("{}: Wrote ROM and reset the system", name);
+    }
+    Operation::Status => {
+      let status = gbl32.get_status()?;
+      info!(
+        "Status: unlocked={}, passthrough={}, reset={}",
+        status.unlocked, status.passthrough, status.reset
+      );
+    }
+  }
+  Ok(())
+}
+
+fn unlock_if_necessary(name: &str, gbl32: &mut Gbl32) -> Result<(), Error> {
+  if gbl32.get_status()?.unlocked {
+    return Ok(());
+  }
+  info!("{}: Unlocking...", name);
+
+  gbl32.set_passthrough(false)?;
+  gbl32.set_unlocked(true)?;
+
+  let mut buffer = vec![0u8; 0x8000];
+  let mut rng = SmallRng::from_entropy();
+  rng.fill_bytes(&mut buffer);
+
+  gbl32.write_all(&buffer)?;
+
+  let data = gbl32.read_all()?;
+
+  for (idx, (a, b)) in buffer.into_iter().zip(data.into_iter()).enumerate() {
+    if a != b {
+      bail!("Self-test failed at index {}", idx);
+    }
+  }
+
+  if !gbl32.get_status()?.unlocked {
+    bail!("Failed to unlock device");
+  }
+  info!("{}: Unlocked device after self-test", name);
+  Ok(())
+}
+
+fn run(matches: &ArgMatches) -> Result<(), Error> {
+  TermLogger::init(LevelFilter::Debug, simplelog::Config::default())?;
+
+  let mut ports = scan_ports()?;
+  if !matches.is_present("broadcast") {
+    if let Some(devices) = matches.values_of_os("port") {
+      ports = devices.map(OsStr::to_os_string).collect();
+    } else if ports.len() > 1 {
+      bail!("Too many detected devices for automatic selection");
+    }
+  }
+
+  if ports.is_empty() {
+    bail!("No supported devices found");
+  }
+
+  info!(
+    "Using {}: {}",
+    if ports.len() == 1 {
+      "device"
+    } else {
+      "devices"
+    },
+    itertools::join(ports.iter().map(|p| p.to_string_lossy()), ", ")
+  );
+
+  let operation = if let Some(path) = matches.value_of_os("upload") {
+    let mut file = File::open(path)?;
+    let mut buf = vec![0; 0x8000];
+    match file.read_exact(&mut buf) {
+      Err(ref e) if e.kind() == io::ErrorKind::UnexpectedEof => {
+        bail!("ROM file is smaller than 32768 bytes");
+      }
+      result => result?,
+    }
+    let mut byte = [0; 1];
+    match file.read_exact(&mut byte) {
+      Err(ref e) if e.kind() == io::ErrorKind::UnexpectedEof => (),
+      Ok(_) => bail!("ROM file is larger than 32768 bytes"),
+      result => result?,
+    }
+    Operation::Upload(buf)
+  } else {
+    Operation::Status
+  };
+
+  let threads = ports
+    .into_iter()
+    .map(|port| {
+      let name = port.to_string_lossy().to_string();
+      let op = operation.clone();
+      (name, thread::spawn(move || worker(&port, op)))
+    })
+    .collect::<Vec<_>>();
+
+  let mut failures = 0;
+  for (name, thread) in threads {
+    let result = thread
+      .join()
+      .map_err(|_| format_err!("{}: failed to wait for worker thread", name))?;
+    if let Err(ref e) = result {
+      let stderr = &mut io::stderr();
+      let _ = writeln!(stderr, "{}: {}\n{}", name, e, e.backtrace());
+      failures += 1;
+    }
+  }
+
+  if failures > 0 {
+    bail!("{} devices failed", failures);
+  }
+  Ok(())
+}
+
+fn main() {
+  let matches = app_from_crate!()
+    .arg(
+      Arg::with_name("broadcast")
+        .short("b")
+        .long("broadcast")
+        .help("Broadcast mode: use all connected devices"),
+    )
+    .arg(
+      Arg::with_name("port")
+        .short("p")
+        .long("port")
+        .value_name("port")
+        .multiple(true)
+        .help("Serial port to use"),
+    )
+    .arg(
+      Arg::with_name("upload")
+        .short("u")
+        .long("upload")
+        .value_name("upload")
+        .help("ROM file to upload"),
+    )
+    .get_matches();
+
+  if let Err(ref e) = run(&matches) {
+    let stderr = &mut io::stderr();
+    let _ = writeln!(stderr, "Error: {}\n{}", e, e.backtrace());
+    process::exit(1);
+  }
+}
